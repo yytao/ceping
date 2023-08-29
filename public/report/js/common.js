8a3fbcc91fcaaaa14ca3e7a00b9ea575a3fea5cc
Taoyy <tyyphp@163.com>
2023-08-29 17:23:53 +0800

    add report style file

---
+++ public/report/js/common.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,c,i){},function(n,c,i){"use strict";i(0)},function(n,c,i){"use strict";i(0)}]]);
---
+++ public/report/js/school.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{4:function(n,w,o){"use strict";o.r(w);o(0),o(1),o(2)}},[[4,1,0]]]);
